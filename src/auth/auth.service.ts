d458b19bd3315c90dcf44593d108cbd864e71420
antaripa04 <antaripapatra99@gmail.com>
2024-11-24 00:57:32 +0530

    add auth dto and class validator

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  signin() {
-    return { msg: 'I have singned in' };
+  signup(dto: AuthDto) {
+    return { msg: 'I have singned up' };
   }
 
-  signup() {
-    return { msg: 'I have singned up' };
+  signin() {
+    return { msg: 'I have singned in' };
   }
 }
