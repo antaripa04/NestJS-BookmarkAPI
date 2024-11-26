a61241148b3b6afef6ac4d0440da59d123ff62c7
antaripa04 <antaripapatra99@gmail.com>
2024-11-27 00:58:17 +0530

    signin logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -32,7 +32,27 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return { msg: 'I have singned in' };
+  async signin(dto: AuthDto) {
+    //find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials Incorrect');
+    }
+    //compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect throw exception
+    if (!pwMatches) {
+      throw new ForbiddenException('Credentials Incorrect');
+    }
+
+    delete user.hash;
+    //send back the user
+    return user;
   }
 }
